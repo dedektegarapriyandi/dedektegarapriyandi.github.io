5a0c529970a3df5983f8cb20d78c00437dec823d
dedektegarapriyandi <dedektegarapriyandi@gmail.com>
2021-10-31 20:50:22 +0700

    create movies content (navbar)

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,21 @@
-// navbar
-const navLinks = document.querySelectorAll(".navbar-link");
-navLinks.forEach((link) => {
-    link.addEventListener("mouseenter", function () {
-        if (!link.classList.contains("active")) {
-            this.classList.add("border-expand");
-        }
+// hover function
+const navHover = (items) => {
+    items.forEach((link) => {
+        link.addEventListener("mouseenter", function () {
+            if (!link.classList.contains("nav-active")) {
+                this.classList.add("border-expand");
+            }
+        });
+        
+        link.addEventListener("mouseleave", function () {
+            this.classList.replace("border-expand", "border-collapse");
+            
+            setTimeout(() => {
+                this.classList.remove("border-collapse");
+            }, 180);
+        });
     });
+}
 
-    link.addEventListener("mouseleave", function () {
-        this.classList.replace("border-expand", "border-collapse");
-
-        setTimeout(() => {
-            this.classList.remove("border-collapse");
-        }, 180);
-    });
-});;
+navHover(document.querySelectorAll(".navbar-link"));
+navHover(document.querySelectorAll(".menu-item"));
